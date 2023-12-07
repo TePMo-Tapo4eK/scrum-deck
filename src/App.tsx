e25469fc66a701583a160cf1fd54561c2e514040
Zakhar <ukhanovza@gmail.com>
2023-12-07 15:00:25 +0300

    Fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 
 import './App.css'
 import { Header } from './components/Header'
-
+declare global {
+  interface Window {
+    Telegram: any;
+  }
+}
 
 function App() {
+  
   const tg = window.Telegram.WebApp
   return (
     <>
