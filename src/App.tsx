4b24e1c4c260f2ce761489895d3cfe7d5d288fa3
Zakhar <ukhanovza@gmail.com>
2023-12-07 14:52:30 +0300

    Try TG webapp

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@
 import './App.css'
 import { Header } from './components/Header'
 
+
 function App() {
+  const tg = window.Telegram.WebApp
   return (
     <>
-    <Header name="novzeal"/>
+    <Header name={tg.initDataUnsafe?.user?.username}/>
       {/* <Header/>
       <Preload/>
       <Category/>
